c8ed4501caed92e7f518d49e1014e022e61e056e
Krish Israni <95641344+krishisrani61@users.noreply.github.com>
2025-03-11 23:20:17 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+{
+  "eventName": "Foujnd Event",
+  "additionalText": "Join us for a special meal",
+  "description": "This is the official online ordering system for the Foujnd event.",
+  "disablePage": false,
+  "redirectUrl": "https://example.com/maintenance",
+  "tables": [1, 2, 3, 4, 5, 6, 7, 8] 
+}
